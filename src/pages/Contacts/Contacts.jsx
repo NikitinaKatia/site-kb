656d4319ce401ba7705ba09d8f5b9beda7092731
Katia Nikitina <nikitinakata973@gmail.com>
2023-03-19 13:05:27 +0300

    create site

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -7,27 +7,32 @@ function Contacts() {
   return(
     <>
       <div className="contactsConteiner">
-        <div className="contactsTextBox1">
-          <p className="contactUs">Свяжитесь с нами</p>
-          {/* <p className="media">Социальные сети</p> */}
-        </div>
       <div className="socialMedia">
+        <p className="contactUs">Свяжитесь с нами</p>
         <div className="socialMediaContainer">
           <div className="sotialMediaImgText">
             <img src='images/iconInstagram.png' alt="instagram icon" className="sotialMediaIcon" />
-            <button className="sotialMediaBtn"><Link to={'https://www.instagram.com/kushai_bucket/?igshid=YmMyMTA2M2Y%3D'}>INSTAGRAM</Link></button>
+            <form action="https://instagram.com/kushai_bucket?igshid=YmMyMTA2M2Y=" target="_blank">
+              <button className="sotialMediaBtn">INSTAGRAM</button>
+            </form>
           </div>
           <div className="sotialMediaImgText">
             <img src='images/iconVk.png' alt="instagram icon" className="sotialMediaIcon" />
-            <button className="sotialMediaBtn"><Link to={'https://vk.com/kushai_bucket'}>VKONTAKTE</Link></button>
+            <form action="https://vk.com/kushai_bucket" target="_blank">
+              <button className="sotialMediaBtn">VKONTAKTE</button>
+            </form>
           </div>
           <div className="sotialMediaImgText">
             <img src='images/iconWhatsapp.png' alt="instagram icon" className="sotialMediaIcon" />
-            <button className="sotialMediaBtn"><Link to={'https://api.whatsapp.com/send?phone=79518480333'}>WHAT'S APP</Link></button>
+            <form action="https://api.whatsapp.com/send?phone=79518480333" target="_blank">
+              <button className="sotialMediaBtn">WHAT'S APP</button>
+            </form>
           </div>
           <div className="sotialMediaImgText">
             <img src='images/iconTelegram.png' alt="instagram icon" className="sotialMediaIcon" />
-            <button className="sotialMediaBtn"><Link to={'https://t.me/+bFGbqkHqu3VmMmRi'}>TELEGRAM</Link></button>
+            <form action="https://t.me/+bFGbqkHqu3VmMmRi" target="_blank">
+              <button className="sotialMediaBtn">TELEGRAM</button>
+            </form>
           </div>
         </div>
       </div>
